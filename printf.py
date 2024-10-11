9ab0820c0963ce4730a6cc2ccde5f5c3b934eeae
aranao00 <150121196+aranao00@users.noreply.github.com>
2024-10-11 16:35:16 +0900

    Update printf.py
    add file name as a parameter of prif function.

--- printf.py
+++ printf.py
@@ -2,7 +2,7 @@ import os
 import config from config_load
 # Loss 및 정확도 기록 용 코드입니다.
 file_name=config['log_path']
-def prif(*args, file_name='/tf/nasw/trainlog.txt', endswith='\n'):
+def prif(*args, file_name=file_name, endswith='\n'):
     string_data=' '.join(map(str, args)) + endswith 
     with open(file_name, 'a') as f:
         f.write(string_data)
