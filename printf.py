70abfb49c3f3a524e3bcf788f634b24b5a82d3f2
aranao00 <150121196+aranao00@users.noreply.github.com>
2024-10-11 16:34:43 +0900

    Update printf.py
    add file name

--- printf.py
+++ printf.py
@@ -1,7 +1,7 @@
 import os
 import config from config_load
 # Loss 및 정확도 기록 용 코드입니다.
-
+file_name=config['log_path']
 def prif(*args, file_name='/tf/nasw/trainlog.txt', endswith='\n'):
     string_data=' '.join(map(str, args)) + endswith 
     with open(file_name, 'a') as f:
